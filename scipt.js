42041efa7a432f7327bff56e51a67dd7ff8fb908
chaturvedi-anil <chaturvedia560@gmail.com>
2023-02-09 16:24:40 +0530

    final Commit

--- scipt.js
+++ scipt.js
@@ -1,19 +1,61 @@
-var current=document.querySelector('.current');
-var next=document.querySelector('.next');
+var countInterval;
 
 function startCounter()
 {
     var number=parseInt(document.getElementById("number").value);
-    alert(number);
-    // var interval=setInterval(animate, 1000);
-    
+    if(isNaN(number))
+    {
+        alert("Please enter a number");
+        clearInterval(countInterval); //This is important for the condition  when a counter is running and you entered a wrong input for a new counter 
+        return;
+    }
+
+    if(number<1 || number>9)
+    {
+        alert("Range out of bounds");
+        clearInterval(countInterval);
+        return;
+    }
+
+    var currentNo=document.querySelector('.current');
+    var nextNo=document.querySelector('.next');
+    var count=0;
+
+    //If user clicks on 'Start Counter' button again 
+    resetNumbers(currentNo, nextNo);
+
+    //clears the previous interval that was running 
+    clearInterval(countInterval);
+
+    countInterval = setInterval(function(){
+        if(count === number)
+        {
+            clearInterval(countInterval);
+            alert("Counter has stopped");
+            return;
+        }
+
+        increaseCount(currentNo, nextNo);
+        count++;
+    }, 1000);
 }
 
-function animate()
+//resetNumber function
+function resetNumbers(currentNo, nextNo)
 {
-    next.classList.add('animate');
+    currentNo.innerText = 0;
+    nextNo.innerText = 1;
+}
+
+//increseCount function
+function increaseCount(currentNo, nextNo)
+{
+    nextNo.classList.add("animate");
+
     setTimeout(function()
     {
-        next.classList.remove('animate');
-    },500);
+        currentNo.innerText = nextNo.innerText;
+        nextNo.classList.remove("animate");
+        nextNo.innerText = parseInt(nextNo.innerText) + 1;
+    }, 500);
 }
